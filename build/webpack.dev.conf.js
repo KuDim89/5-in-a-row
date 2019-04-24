a4413c27e98c063d4ca57e4fef5c09d88bdda765
dkukharuk <dkukharuk@corevalue.net>
2019-04-24 11:43:34 +0300

    add menu and game field to content

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -5,7 +5,7 @@ const baseWebpackConfig = require('./webpack.base.conf');
 const devWebpackConfig = merge(baseWebpackConfig, {
     //DEV config
     mode: 'development',
-    //devtool: 'cheap-module-eval-source-map',
+    //devtool: 'cheap-modules-eval-source-map',
     devServer: {
         contentBase: baseWebpackConfig.externals.path.dist,
         // historyApiFallback: true,
--- src/js/common.js
+++ src/js/common.js
@@ -1,1 +1,2 @@
 //Js scripts
+import "./modules/create-game-field"
