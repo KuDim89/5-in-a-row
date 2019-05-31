0b0e7705bb02e5fefc441dfad4839c2d17918bf6
dkukharuk <dkukharuk@corevalue.net>
2019-05-31 18:37:28 +0300

    add new version visualization on board

--- src/js/common.js
+++ src/js/common.js
@@ -1,4 +1,9 @@
 //Js scripts
-import "./modules/create-game-field"
-import "./modules/add-gifure-on-field"
-import "./modules/create-local-storage"
+//import "./modules/create-game-field"
+//import "./modules/add-figure-on-field"
+//import "./modules/create-local-storage"
+
+//import "./modules/createBoardOnPage"
+import "./modules/createSourceLocallStorageObj";
+import "./modules/addFigureOnBoard";
+import "./modules/clickOnButton";
